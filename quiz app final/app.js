6235a3e147f70f3e0194983dabca8188fce87c06
shubham chaurasia <130426868+sc2211@users.noreply.github.com>
2023-12-16 16:09:23 +0530

    Add files via upload

---
+++ quiz app final/app.js
@@ -0,0 +1,549 @@
+const Pipes_And_cisterns= [
+    {
+        'que': 'Pipe A can fill a tank in 4 hours, while Pipe B can fill the same tank in 6 hours. How long will it take for both pipes to fill the tank together?',
+        'a': '2 hours',
+        'b': '2.4 hours',
+        'c': '3 hours',
+        'd': '3.6 hours',
+        'correct': 'b'
+    },
+    {
+        'que': 'Pipe X can fill a tank in 8 hours, and Pipe Y can empty the same tank in 12 hours. If both pipes are opened simultaneously, how long will it take to fill the tank?',
+        'a': '4.8 hours',
+        'b': '6 hours',
+        'c': '9 hours',
+        'd': '16 hours',
+        'correct': 'a'
+    },
+    {
+        'que': 'Pipe A can fill a tank in 5 hours, and Pipe B can empty the same tank in 8 hours. If both pipes are opened together, how long will it take to fill the tank?',
+        'a': '5.33 hours',
+        'b': '7.14 hours',
+        'c': '8 hours',
+        'd': '8.75 hours',
+        'correct': 'b'
+    },
+    {
+        'que': 'Pipe P can fill a tank in 10 hours, and Pipe Q can fill the same tank in 15 hours. If Pipe R can empty the tank in 6 hours, how long will it take for all three pipes to fill the tank together?',
+        'a': '3 hours',
+        'b': '4 hours',
+        'c': '5 hours',
+        'd': '6 hours',
+        'correct': 'b'
+    },
+    {
+        'que': 'Pipe A can fill a tank in 3 hours, and Pipe B can fill the same tank in 6 hours. If Pipe C can empty the tank in 9 hours, how long will it take for all three pipes to fill the tank together?',
+        'a': '1.8 hours',
+        'b': '2.4 hours',
+        'c': '3 hours',
+        'd': '4.5 hours',
+        'correct': 'b'
+    },
+    {
+        'que': 'Pipe A can fill a tank in 4 hours, and Pipe B can empty the same tank in 6 hours. If both pipes are opened together, how long will it take to empty the tank?',
+        'a': '4.8 hours',
+        'b': '6 hours',
+        'c': '9 hours',
+        'd': '24 hours',
+        'correct': 'a'
+    },
+    {
+        'que': 'Pipe X can fill a tank in 12 hours, and Pipe Y can empty the same tank in 9 hours. If both pipes are opened simultaneously, how long will it take to empty the tank?',
+        'a': '4.8 hours',
+        'b': '6 hours',
+        'c': '9 hours',
+        'd': '36 hours',
+        'correct': 'c'
+    },
+    {
+        'que': 'Pipe A can fill a tank in 6 hours, and Pipe B can empty the same tank in 8 hours. If both pipes are opened together, how long will it take to empty the tank?',
+        'a': '4.8 hours',
+        'b': '6 hours',
+        'c': '8 hours',
+        'd': '9.6 hours',
+        'correct': 'd'
+    },
+    {
+        'que': 'Pipe P can fill a tank in 10 hours, and Pipe Q can fill the same tank in 15 hours. If Pipe R can empty the tank in 12 hours, how long will it take for all three pipes to fill the tank together?',
+        'a': '3.33 hours',
+        'b': '4 hours',
+        'c': '5 hours',
+        'd': '6 hours',
+        'correct': 'c'
+    },
+    {
+        'que': 'Pipe A can fill a tank in 3 hours, and Pipe B can empty the same tank in 6 hours. If Pipe C can empty the tank in 9 hours, how long will it take for all three pipes to empty the tank together?',
+        'a': '1.8 hours',
+        'b': '2.4 hours',
+        'c': '3 hours',
+        'd': '4.5 hours',
+        'correct': 'd'
+    }
+]
+const Probability = [
+    {
+        'que': 'What is the probability of rolling a six on a fair six-sided die?',
+        'a': '1/2',
+        'b': '1/6',
+        'c': '1/3',
+        'd': '1/4',
+        'correct': 'b'
+    },
+    {
+        'que': 'If two fair coins are tossed, what is the probability of getting at least one head?',
+        'a': '1/2',
+        'b': '1/3',
+        'c': '3/4',
+        'd': '3/8',
+        'correct': 'c'
+    },
+    {
+        'que': 'A deck of playing cards contains 52 cards. What is the probability of drawing a red card?',
+        'a': '1/2',
+        'b': '1/4',
+        'c': '1/3',
+        'd': '1/5',
+        'correct': 'a'
+    },
+    {
+        'que': 'In a bag, there are 5 red marbles, 3 blue marbles, and 2 green marbles. What is the probability of drawing a blue marble?',
+        'a': '1/10',
+        'b': '3/10',
+        'c': '1/5',
+        'd': '2/5',
+        'correct': 'b'
+    },
+
+    {
+        'que': 'A spinner is divided into 8 equal sections numbered from 1 to 8. What is the probability of landing on an even number?',
+        'a': '1/4',
+        'b': '1/2',
+        'c': '3/8',
+        'd': '5/8',
+        'correct': 'b'
+    },
+    {
+        'que': 'In a class, there are 25 boys and 15 girls. If a student is randomly selected, what is the probability of selecting a boy?',
+        'a': '1/2',
+        'b': '5/8',
+        'c': '5/9',
+        'd': '3/8',
+        'correct': 'b'
+    },
+    {
+        'que': 'A bag contains 4 red balls and 6 green balls. If two balls are drawn without replacement, what is the probability of drawing two green balls?',
+        'a': '1/5',
+        'b': '3/10',
+        'c': '3/8',
+        'd': '1/3',
+        'correct': 'c'
+    },
+    {
+        'que': 'A box contains 10 chocolates, of which 4 are dark chocolates and 6 are milk chocolates. If two chocolates are randomly selected with replacement, what is the probability of getting a dark chocolate and then a milk chocolate?',
+        'a': '2/5',
+        'b': '3/10',
+        'c': '2/15',
+        'd': '4/15',
+        'correct': 'd'
+    },
+    {
+        'que': 'A fair six-sided die is rolled twice. What is the probability of getting a sum of 7?',
+        'a': '1/12',
+        'b': '1/6',
+        'c': '1/3',
+        'd': '5/36',
+        'correct': 'd'
+    },
+    {
+        'que': 'In a bag, there are 8 blue marbles and 4 red marbles. If two marbles are drawn without replacement, what is the probability of drawing a blue marble and then a red marble?',
+        'a': '4/33',
+        'b': '8/33',
+        'c': '1/6',
+        'd': '2/9',
+        'correct': 'b'
+    }
+]
+let Problems_on_Age = [
+    {
+        'que': 'The sum of the present ages of John and Lisa is 48. Five years ago, John was three times as old as Lisa. What is Lisas present age?',
+'a': '12 years',
+        'b': '15 years',
+        'c': '18 years',
+        'd': '20 years',
+        'correct': 'c'
+    },
+    {
+        'que': 'Ten years ago, the ratio of Alices age to Bobs age was 3:5. If the sum of their present ages is 56, how old is Alice now?',
+        'a': '20 years',
+        'b': '24 years',
+        'c': '28 years',
+        'd': '32 years',
+        'correct': 'd'
+    },
+    {
+        'que': 'The present age of a father is three times the age of his son. If the sum of their ages is 48, what is the son`s present age?',
+'a': '12 years',
+        'b': '14 years',
+        'c': '16 years',
+        'd': '18 years',
+        'correct': 'a'
+    },
+    {
+        'que': 'Five years ago, the ratio of Sarah`s age to her daughter`s age was 4:1. If the sum of their present ages is 40, how old is Sarah?',
+        'a': '24 years',
+        'b': '28 years',
+        'c': '32 years',
+        'd': '36 years',
+        'correct': 'b'
+    },
+    {
+        'que': 'The sum of the ages of three friends is 72. If the ratio of their ages is 2:3:4, how old is the youngest friend?',
+        'a': '12 years',
+        'b': '16 years',
+        'c': '20 years',
+        'd': '24 years',
+        'correct': 'a'
+    },
+    {
+        'que': 'Ten years ago, the age of a father was four times the age of his son. If the father is now twice as old as his son, what is the present age of the son?',
+        'a': '10 years',
+        'b': '15 years',
+        'c': '20 years',
+        'd': '25 years',
+        'correct': 'b'
+    },
+    {
+        'que': 'The sum of the present ages of Jane and Mark is 30. Four years ago, the ratio of their ages was 3:2. How old is Jane now?',
+        'a': '8 years',
+        'b': '10 years',
+        'c': '12 years',
+        'd': '14 years',
+        'correct': 'c'
+    },
+    {
+        'que': 'Five years ago, the age of a mother was seven times the age of her daughter. If the present age of the mother is 35, what is the present age of the daughter?',
+        'a': '5 years',
+        'b': '7 years',
+        'c': '9 years',
+        'd': '11 years',
+        'correct': 'b'
+    },
+    {
+        'que': 'The sum of the ages of a grandfather and his grandson is 100. The grandfather is five times older than the grandson. How old is the grandson?',
+        'a': '10 years',
+        'b': '12 years',
+        'c': '15 years',
+        'd': '20 years',
+        'correct': 'a'
+    },
+    {
+        'que': 'Three years ago, the age of a father was four times the age of his son. If the father is now three times as old as his son, what is the present age of the son?',
+        'a': '6 years',
+        'b': '9 years',
+        'c': '12 years',
+        'd': '15 years',
+        'correct': 'c'
+    }
+]
+let Profit_And_Loss = [
+    {
+        'que': 'A shopkeeper sells a shirt for ₹500, making a profit of 25%. What was the cost price of the shirt?',
+        'a': '₹400',
+        'b': '₹450',
+        'c': '₹550',
+        'd': '₹600',
+        'correct': 'b'
+    },
+    {
+        'que': 'A bike is sold for ₹12000 at a loss of 20%. What was the cost price of the bike?',
+        'a': '₹9000',
+        'b': '₹10000',
+        'c': '₹13000',
+        'd': '₹15000',
+        'correct': 'c'
+    },
+    {
+        'que': 'A trader bought 100 pens for ₹200. If he sells each pen for ₹3, what is his percentage profit?',
+        'a': '50%',
+        'b': '100%',
+        'c': '150%',
+        'd': '200%',
+        'correct': 'b'
+    },
+    {
+        'que': 'A product is marked for sale at ₹800, which includes a markup of 25% on the cost price. What is the cost price of the product?',
+        'a': '₹600',
+        'b': '₹640',
+        'c': '₹660',
+        'd': '₹700',
+        'correct': 'c'
+    },
+    {
+        'que': 'A laptop is bought for ₹8000 and sold for ₹7000. What is the loss percentage?',
+        'a': '12.5%',
+        'b': '10%',
+        'c': '7.5%',
+        'd': '5%',
+        'correct': 'a'
+    },
+    {
+        'que': 'A retailer buys a box of chocolates for ₹500 and sells it at a markup of 20%. If he sells each chocolate for ₹3, how many chocolates are there in the box?',
+        'a': '16',
+        'b': '20',
+        'c': '24',
+        'd': '30',
+        'correct': 'c'
+    },
+    {
+        'que': 'A pair of shoes is sold for ₹600, making a profit of 20%. What was the cost price of the shoes?',
+        'a': '₹450',
+        'b': '₹480',
+        'c': '₹500',
+        'd': '₹520',
+        'correct': 'b'
+    },
+    {
+        'que': 'A watch is sold at a discount of 15% for ₹850. What was the marked price of the watch?',
+        'a': '₹900',
+        'b': '₹1000',
+        'c': '₹1050',
+        'd': '₹1150',
+        'correct': 'c'
+    },
+    {
+        'que': 'A vendor bought a box of apples for ₹400. If he wants to make a 25% profit, at what price should he sell each apple?',
+        'a': '₹0.50',
+        'b': '₹0.60',
+        'c': '₹0.75',
+        'd': '₹1.00',
+        'correct': 'c'
+    },
+    {
+        'que': 'A car is sold for ₹150000, resulting in a loss of 10%. What was the cost price of the car?',
+        'a': '₹165000',
+        'b': '₹165000',
+        'c': '₹165000',
+        'd': '₹165000',
+        'correct': 'd'
+    }
+]
+let option;
+let option1;
+let total =10
+let index = 0;
+let right = 0,
+    wrong = 0;
+let attemt=right+wrong;
+let i=0;
+let question_Type = 0;
+const time=document.getElementById("time_taken")
+const score = document.getElementById("score")
+const Que_type = document.getElementById("Que_type_Name")
+const QuesBox = document.getElementById("QuesBox")
+const que_number = document.getElementById("que_number")
+const options = document.querySelectorAll('.options')
+
+// /////function to load questions/////////
+setInterval(()=>{
+    if(i<=600){
+        
+        document.getElementById("timer").innerText=i
+        localStorage.setItem('time', i);
+                let time = localStorage.getItem('type');
+              
+
+                
+    }
+    i++;
+},1000);
+/////////////function to load questions////////////
+const loadquestion = () => {
+    
+  
+    if (index == total) {
+        return endQuiz()
+    }
+    reset()
+///////////if condition to print the question type/////////////
+    if(Qtype==1)
+    var data = Pipes_And_cisterns[index]
+    
+    
+    if(Qtype==2)
+    var data = Probability[index]
+    
+    if(Qtype==3)
+    var data = Problems_on_Age[index]
+    
+    if(Qtype==4)
+    var data = Profit_And_Loss[index]
+    option1=data.correct;
+    QuesBox.innerText = `${data.que}`;
+    que_number.innerText = index + 1
+    score.innerText=right
+
+    options[0].nextElementSibling.innerText = data.a;
+    options[1].nextElementSibling.innerText = data.b;
+    options[2].nextElementSibling.innerText = data.c;
+    options[3].nextElementSibling.innerText = data.d;
+}
+
+
+// }
+////////////    submit button       //////////////////////
+const submitQuiz = () => {
+    if(Qtype==1)
+    var data = Pipes_And_cisterns[index]
+    
+    
+    if(Qtype==2)
+    var data = Probability[index]
+    
+    if(Qtype==3)
+    var data = Problems_on_Age[index]
+    
+    if(Qtype==4)
+    var data = Profit_And_Loss[index]
+  
+    let ans = getAnswer()
+    if (ans ==data.correct) {
+        right++;
+       
+    }
+    else {
+        wrong++;
+    }
+    
+    index++;
+    set_result();
+    document.getElementById("a").style.backgroundColor='rgba(27, 145, 255,0.3)'; 
+    document.getElementById("b").style.backgroundColor='rgba(27, 145, 255,0.3)'; 
+    document.getElementById("c").style.backgroundColor='rgba(27, 145, 255,0.3)'; 
+    document.getElementById("d").style.backgroundColor='rgba(27, 145, 255,0.3)'; 
+    loadquestion();
+
+    return;
+}
+//////////fuction question type name//////////
+const Q_Type_Heading=()=>{
+
+    
+if(Qtype==1)
+ Que_type.innerText=('Pipes And Cisterns');
+
+if(Qtype==2)
+Que_type.innerText=('Probability');
+if(Qtype==3)
+Que_type.innerText=('Problems_on_Age');
+if(Qtype==4)
+Que_type.innerText=('rofit_And_Loss');
+}
+///////////function to check selection///////
+const getAnswer = () => {
+    let answer;
+    options.forEach(
+        (input) => {
+            if (input.checked) {
+                answer = input.value;
+            }
+        }
+    )
+    return answer;
+    
+}
+/////////////////function to reset selection of previous question///////////////
+
+const reset = () => {
+    options.forEach(
+        (input) => {
+            input.checked = false
+        }
+    )
+
+}
+//////////////function to end quiz after last que and show result page///////// 
+const endQuiz = () => {
+    window.location.href = 'result.html';
+    result();
+
+}
+
+    //////////result////////
+    set_result=()=>{
+
+    localStorage.setItem('Wrong', wrong);
+    localStorage.setItem('Correct', right);
+    }
+    /////////////////functions to chnage color in click//////////
+clr_on_click1=()=>
+{ 
+        options.forEach(
+            (input) => {
+                if (input.checked) {
+                    option = input.value;
+                }
+            }
+        ) 
+    if (option ==option1) {
+        document.getElementById("a").style.backgroundColor='green';   
+    }
+    else {
+        document.getElementById("a").style.backgroundColor='red';
+    }
+}
+
+clr_on_click2=()=>
+{ 
+        options.forEach(
+            (input) => {
+                if (input.checked) {
+                    option = input.value;
+                }
+            }
+        ) 
+    if (option ==option1) {
+        document.getElementById("b").style.backgroundColor='green';   
+    }
+    else {
+        document.getElementById("b").style.backgroundColor='red';
+    }
+}
+clr_on_click3=()=>
+{ 
+        options.forEach(
+            (input) => {
+                if (input.checked) {
+                    option = input.value;
+                }
+            }
+        ) 
+    if (option ==option1) {
+        document.getElementById("c").style.backgroundColor='green';   
+    }
+    else {
+        document.getElementById("c").style.backgroundColor='red';
+    }
+}
+clr_on_click4=()=>
+{ 
+        options.forEach(
+            (input) => {
+                if (input.checked) {
+                    option = input.value;
+                }
+            }
+        ) 
+    if (option ==option1) {
+        document.getElementById("d").style.backgroundColor='green';   
+    }
+    else {
+        document.getElementById("d").style.backgroundColor='red';
+    }
+}
+
+
+Qtype=localStorage.getItem('type');
+Q_Type_Heading();
+loadquestion();
+;
+
